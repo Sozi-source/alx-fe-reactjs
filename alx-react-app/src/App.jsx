855097aaa98631bfff3a1bb5b88a83ec4f5bbcb5
Wilfred Osozi <osoziw@gmail.com>
2025-02-16 02:07:00 +0300

    add the app components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from '../Header'
+import MainContent from '../MainContent'
+import Footer from '../Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,7 +31,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p><WelcomeMessage />
+      </p>
+      <WelcomeMessage />
+      <Header/>
+      <MainContent/>
+      <Footer/>
 
     </>
   )
