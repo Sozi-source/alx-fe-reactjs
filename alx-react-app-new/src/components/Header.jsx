b764796ccc74f08506964c000008c679fad09f45
Wilfred Osozi <osoziw@gmail.com>
2025-02-17 22:38:06 +0300

    duplicate the app

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header(){
+
+    return (
+
+<header style={{backgroundColor: 'maroon', color: 'greenyellow', textAlign: 'center'}}>
+  <h1>My Favorite Cities</h1>
+</header>
+
+    )
+}
+
+export default Header;
+
+
