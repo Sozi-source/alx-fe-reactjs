6b6fd245d1ae7001c81bc4a7e3c53cad16d0f949
Wilfred Osozi <osoziw@gmail.com>
2025-02-17 23:20:09 +0300

    style color blue

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ const UserProfile = (props) => {
     return (
       <div style={{border:'1px solid gray', padding:'10px', margin: '10px'}}>
         <h1 style={{fontSize: '30px', alignItems:'center', color:'purple'}}>User profile</h1>
-        <h2 style={{fontWeight:'bold', backgroundColor: 'red'}}>{props.name}</h2>
+        <h2 style={{fontWeight:'bold', color: 'blue'}}>{props.name}</h2>
         <p>Age: <span style={{fontWeight:'bold'}}></span>{props.age}</p>
         <p style={{marginTop: '20px', backgroundColor: 'pink',}}>Bio: {props.bio}</p>
       </div>
