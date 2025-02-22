a3ecd1027a6f666dec608a3dc7f12624619e7bd3
Wilfred Osozi <osoziw@gmail.com>
2025-02-23 01:59:51 +0300

    updated the links

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar(){
 
     return (
 
-        <nav>
-            <ul>
-                <li><link to = "/"></link>Home</li>
-                <li><link to = "/"></link>About</li>
-                <li><link to = "/"></link>Service</li>
-                <li><link to = "/"></link>Contacts</li>
+        <nav style={{ background: "white", padding: "10px", textAlign: "center" }}>
+            <ul style={{ listStyle: "none", display: "flex", justifyContent: "center", gap: "20px" }}> 
+                <li ><Link to = "/" style={{ color: "blue", textDecoration: "none" }}>Home</Link></li>
+                <li><Link to = "/about" style={{ color: "blue", textDecoration: "none" }}>About</Link></li>
+                <li><Link to = "/services" style={{ color: "blue", textDecoration: "none" }}>Service</Link></li>
+                <li><Link to = "/contact" style={{ color: "blue", textDecoration: "none" }}>Contacts</Link></li>
             </ul>
         </nav>
     )
