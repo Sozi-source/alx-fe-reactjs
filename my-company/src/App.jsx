b8746140ecf58d1b0dbb18485cce4715e2c0c5e7
Wilfred Osozi <osoziw@gmail.com>
2025-02-23 01:39:03 +0300

    changes made

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-import 
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+
+
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+      <Routes>
+        <Route path = "/" element = {<Home/>} />
+        <Route path = "/" about = {<About/>} />
+        <Route path = "/" service = {<Services/>} />
+        <Route path = "/" Contacts = {<Contacts/>} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar(){
+
+    return (
+
+        <nav>
+            <ul>
+                <li><link to = "/"></link>Home</li>
+                <li><link to = "/"></link>About</li>
+                <li><link to = "/"></link>Service</li>
+                <li><link to = "/"></link>Contacts</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
