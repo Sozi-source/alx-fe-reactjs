7c2f46198a12bffca2d94c4d2b4795929f3cec07
Wilfred Osozi <osoziw@gmail.com>
2025-02-23 02:07:23 +0300

    adjusted navbar background color

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar(){
 
     return (
 
-        <nav style={{ background: "white", padding: "10px", textAlign: "center" }}>
+        <nav style={{ backgroundColor: "pink", padding: "10px", textAlign: "center" }}>
             <ul style={{ listStyle: "none", display: "flex", justifyContent: "center", gap: "20px" }}> 
-                <li ><Link to = "/" style={{ color: "blue", textDecoration: "none" }}>Home</Link></li>
-                <li><Link to = "/about" style={{ color: "blue", textDecoration: "none" }}>About</Link></li>
-                <li><Link to = "/services" style={{ color: "blue", textDecoration: "none" }}>Service</Link></li>
-                <li><Link to = "/contact" style={{ color: "blue", textDecoration: "none" }}>Contacts</Link></li>
+                <li ><Link to = "/" style={{ color: "black", textDecoration: "none", backgroundColor: "greenyellow"}}>Home</Link></li>
+                <li><Link to = "/about" style={{ color: "black", textDecoration: "none", backgroundColor: "greenyellow" }}>About</Link></li>
+                <li><Link to = "/services" style={{ color: "black", textDecoration: "none", backgroundColor: "greenyellow" }}>Service</Link></li>
+                <li><Link to = "/contact" style={{ color: "black", textDecoration: "none", backgroundColor: "greenyellow" }}>Contacts</Link></li>
             </ul>
         </nav>
     )
