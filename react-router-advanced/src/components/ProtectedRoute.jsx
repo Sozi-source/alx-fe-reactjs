edad2d284ef5b5fc083290faf3ac2d2e6def864f
Wilfred Osozi <osoziw@gmail.com>
2025-03-09 22:07:54 +0300

    use Auth

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "./useAuth";
 
 function ProtectedRoute() {
-    const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-    return isAuthenticated ? <Outlet/>: <Navigate to="/login" />;
+    const isAuthenticated = useAuth();
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
