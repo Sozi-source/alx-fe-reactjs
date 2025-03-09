1f0abdbde031a0ef67bd73469001b11e16a5647e
Wilfred Osozi <osoziw@gmail.com>
2025-03-09 12:50:56 +0300

    added routing in profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,9 +1,23 @@
+import {Routes, Route } from "react-router-dom";
+import ProfileDetails from "../Pages/Profile/ProfileDetails";
+import ProfileSettings from "../Pages/Profile/ProfileSettings";
+
+
 
 function Profile(){
 
     return(
         <div>
             <h1>PROFILE PAGE</h1>
+
+
+    // Routes to child components
+        <Routes>
+            <Route path="/profiledetails" Component={ProfileDetails} />
+            <Route path="/profilesettings" Component={ProfileSettings} />
+
+        </Routes>
+
         </div>
     )
 }
