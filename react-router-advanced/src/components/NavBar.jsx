46af0ee5f50c7f9d53126970827e701ef5caec69
Wilfred Osozi <osoziw@gmail.com>
2025-03-09 14:25:35 +0300

    link to blogs

--- react-router-advanced/src/components/NavBar.jsx
+++ react-router-advanced/src/components/NavBar.jsx
@@ -9,6 +9,7 @@ return(
             <li><Link to= "/about">About</Link></li>
             <li><Link to= "/contact">Contact</Link></li>
             <li><Link to= "/profile">Profile</Link></li>
+            <li><Link to= "/blog/:id">Blogs</Link></li>
         </nav>
     </div>
 )}
