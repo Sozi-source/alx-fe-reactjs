4399bbabd7ad2c16a0f181aa87c36a24a185776e
Wilfred Osozi <osoziw@gmail.com>
2025-03-15 19:46:59 +0300

    jest

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,6 +1,8 @@
-export default {
+module.exports = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
-  }
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  moduleDirectories: ["node_modules", "src"]
 };
