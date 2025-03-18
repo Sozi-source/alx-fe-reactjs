c8e2de9e402d902f1d4d6f7fd26ecb0ca717c169
Wilfred Osozi <osoziw@gmail.com>
2025-03-18 22:30:50 +0300

    set up tailwind css

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,12 @@
-import '../id'
+import React from 'react'
+import './index.css'
 
-function App(){
-
-  return(
+function App() {
+  return (
     <div>
-      <h1 className="text-blue-500">text-blue-500</h1>
+      <h1 className='text-blue-500 text-center pt-10 font-bold'>Recipe Sharing Platform</h1>
     </div>
   )
 }
-export default App;
+
+export default App
