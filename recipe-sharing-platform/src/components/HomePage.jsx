b0155f59c21f4ead6d6f0c6fb83193915e517b16
Wilfred Osozi <osoziw@gmail.com>
2025-03-19 01:10:51 +0300

    styled

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -7,15 +7,15 @@ function HomePage() {
 const[recipes, setRecipes]= useState([]);
 
 useEffect(()=>{
-    import("../data.json") // Import dynamically
+    import("../data.json") 
       .then((module) => setRecipes(module.default))
       .catch((error) => console.error("Error loading JSON:", error));
 },[])
   
     
     return (
-    <div>
-        <h1 className="text-center font-bold text-2xl">Recipe List</h1>
+    <div className="rounded-2xl shadow-lg">
+        <h1 className="text-center font-bold text-2xl hover:bg-green-500">Recipe List</h1>
         {recipes.map((recipe)=>(
            <div key={recipe.id}>
             <p className="text-center text-blue-700">{recipe.title}</p>
