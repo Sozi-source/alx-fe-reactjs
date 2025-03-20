bc5cc46fb8ff7ab53f216f25be55361416328adb
Wilfred Osozi <osoziw@gmail.com>
2025-03-21 00:11:33 +0300

    added images to recipes

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -24,6 +24,7 @@ if(!recipe) return <p>Recipe Loading...</p>
         <div className='max-w-md mx-auto px-6 py-6 bg-rose-200 shadow-xl rounded-2xl border border-gray-200 mt-5'>
             <button onClick={()=>navigate(-1)} className='mb-4 px-4 bg-blue-300 text-gray-800 rounded-lg hover:bg-gray-400 transition'>Back</button>
             
+            <img src='recipe.image' alt='recipe.title'/>
             <p className='text-2xl font-bold text-gray-800 mb-2'>{recipe.title}</p>
 
             <h1 className='text-lg font-bold text-gray-700 mt-4 gap-2'>Ingredients</h1>
