bf5aa47d62df67df43e37c19c1f17238e85c1e2c
Wilfred Osozi <osoziw@gmail.com>
2025-03-22 15:54:11 +0300

    added validate errors

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -5,19 +5,19 @@ function AddRecipeForm() {
 const[title, setTitle]=useState("")
 const[ingredients, setIngredients]= useState("")
 const[steps, setSteps]= useState("")
-const[error, setError] =useState({})
+const[errors, setErrors] =useState({})
 
 const handleSubmit=(e)=>{
     e.preventDefault()
 
     // Validation Error
-const validationError ={}
-if(!title.trim())validationError.title= "Title required"
-if(!ingredients.trim())validationError.ingredients = "Ingredients required"
-if(!steps.trim())validationError.steps = "Preparation steps required"
+const validate ={}
+if(!title.trim())validate.title= "Title required"
+if(!ingredients.trim())validate.ingredients = "Ingredients required"
+if(!steps.trim())validate.steps = "Preparation steps required"
 
-if (Object.keys(validationError).length > 0){
-    setError(validationError);
+if (Object.keys(validate).length > 0){
+    setErrors(validate);
     return
 }
 
@@ -31,7 +31,7 @@ console.log("New Recipes", newRecipe)
     setTitle("");
     setIngredients("");
     setSteps("");
-    setError({});
+    setErrors({});
 }
 
 return (
@@ -41,21 +41,21 @@ return (
                 <label htmlFor="title">Title</label><br />
             <textarea name="title" id="title" value={title} onChange={(e)=>setTitle(e.target.value)}
             className='border border-gray-400'></textarea> <br />
-            {error?.title && <p className='text-red-500'>{error.title}</p>}
+            {errors?.title && <p className='text-red-500'>{errors.title}</p>}
             </div>
 
             <div >
                 <label htmlFor="ingredient">Ingredients</label><br />
             <textarea name="ingredients" id="ingredient" value={ingredients} onChange={(e)=>setIngredients(e.target.value)}
             className='border border-gray-400'></textarea> <br />
-            {error?.ingredients && <p className='text-red-500'>{error.ingredients}</p>}
+            {errors?.ingredients && <p className='text-red-500'>{errors.ingredients}</p>}
             </div>
 
             <div>
                 <label htmlFor="preparation">Preparation Steps</label><br />
             <textarea name="preparation" id="preparation" value={steps} onChange={(e)=>setSteps(e.target.value)}
             className='border border-gray-400 mt'></textarea> <br />
-            {error?.steps && <p className='text-red-500'>{error.steps}</p>}
+            {errors?.steps && <p className='text-red-500'>{errors.steps}</p>}
             </div>
             
             <button type='submit' className=' rounded-md mt-5 border border-black bg-blue-400 hover:bg-red-500 w-20 h-12'>Submit</button>   
