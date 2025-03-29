e3a0108b1560f0852fa0e13769ff202bffbd3238
Wilfred Osozi <osoziw@gmail.com>
2025-03-29 15:51:08 +0300

    added a search component

--- github-user-search/src/components/SearchBar.jsx
+++ github-user-search/src/components/SearchBar.jsx
@@ -1,31 +1,77 @@
 import React from 'react'
 import { useState } from 'react'
+import { fetchUserData } from '../services/githubService';
 
 const SearchBar =({onSearch})=> {
 
 const[username, setUsername]=useState("");
+const[userData, setUserData]=useState(null);
+const[error, setError]=useState(null);
 
-const handleSubmit=(e)=>{
+
+const handleSubmit = async(e)=>{
 e.preventDefault()
-if(username.trim()){
-    onSearch(username);
-    setUsername("")
-}
+if(!username.trim()) return;
+
+try{
+  setError (null);
+const data = await fetchUserData(username);
+setUserData(data) 
 
 }
 
+catch (err){
+  setUserData(null)
+  setError("User does not exist!!")
+}
 
-  return (
-    <div className='flex justify-center items-center h-screen'>
-        <form onSubmit={handleSubmit} className='flex flex-col'> 
-            <input type="text" placeholder='Enter github username' 
-            value={username} onChange={(e)=>setUsername(e.target.value)} className='border border-gray-500 p-3 rounded-lg text-center'/>
+
+}
+
+return (
+  <div className='flex flex-col justify-center items-center h-screen'>
+      <form onSubmit={handleSubmit}> 
+          <input type="text" placeholder='Enter github username' 
+          value={username} onChange={(e)=>setUsername(e.target.value)} className='border border-gray-500 p-3 rounded-lg text-center'/>
+      
+          <button type='submit' className='mt-5 bg-blue-400 hover:bg-blue-700 w-fit mx-auto p-3 rounded-lg ml-5'>Search</button>
+      </form>
+
+      {/* Display Error */}
+      
+      {error && (
+        <p className='text-red-600 mt-4 animate-bounce font-bold'>{error} </p>
+      )}
+      
+
+      {/* Display UserData */}
+     
+      {userData &&(
         
-            <button type='submit' className='mt-5 bg-blue-400 hover:bg-blue-700 w-fit mx-auto p-3 rounded-lg'>Search</button>
-        </form>
+        <div className='mt-6 p-6 border rounded-lg shadow-lg text-center w-80 bg-green-100'>
 
-    </div>
-  )
-}
+         <img
+            src={userData.avatar_url}
+            alt={userData.login}
+            className="w-24 h-24 rounded-full mx-auto"
+          />
+
+        <h2 className='mt-2 text-xl font-bold'>{userData.name || userData.login} </h2>
+          <p className="text-gray-600">@{userData.login}</p>
+          <p>Public Repos: {userData.public_repos}</p>
+          <p>Followers: {userData.followers} | Following: {userData.following}</p>
+
+          <a href={userData.html_url} target='_blank' className="block mt-2 text-blue-500 hover:underline">View profile</a>
+        </div>
+      )}
+
+       
+  </div>
+)}
 
 export default SearchBar;
+
+
+
+
+  
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const GITHUB_API_URL= import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com/users/"
+
+
+export const fetchUserData = async (username) => {
+    try {
+      const response = await axios.get(`${GITHUB_API_URL}${username}`, {
+       
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error("User not found or API limit exceeded");
+    }
+  };
