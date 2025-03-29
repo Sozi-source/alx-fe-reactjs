656ab939624edc6af4ab79c40cdb589653dc7b83
Wilfred Osozi <osoziw@gmail.com>
2025-03-29 14:50:50 +0300

    remove the git user fetch component

--- github-user-search/src/services/gitHubApi.jsx
+++ github-user-search/src/services/gitHubApi.jsx
@@ -1,15 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
-const GITHUB_API_URL= import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com/users/"
+// const GITHUB_API_URL= import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com/users/"
 
 
-export const fetchGitHubUser = async (username) => {
-    try {
-      const response = await axios.get(`${GITHUB_API_URL}${username}`, {
+// export const fetchGitHubUser = async (username) => {
+//     try {
+//       const response = await axios.get(`${GITHUB_API_URL}${username}`, {
        
-      });
-      return response.data;
-    } catch (error) {
-      throw new Error("User not found or API limit exceeded");
-    }
-  };
+//       });
+//       return response.data;
+//     } catch (error) {
+//       throw new Error("User not found or API limit exceeded");
+//     }
+//   };
