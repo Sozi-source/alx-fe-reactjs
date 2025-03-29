7526a3864ae13c6f563671ba10ad8efc6b6be699
Wilfred Osozi <osoziw@gmail.com>
2025-03-29 23:50:41 +0300

    updated the api url by adding ?q

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,47 +1,6 @@
-// import axios from "axios";
-
-// const GITHUB_API_URL = "https://api.github.com/search/users";
-// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
-// export const fetchUserData = async (username, location, minRepos, page = 1) => {
-//   try {
-//     const query = `${username} ${location ? `location:${location}` : ""} ${
-//       minRepos ? `repos:>${minRepos}` : ""
-//     }`;
-
-//     const response = await axios.get(`${GITHUB_API_URL}?q=${encodeURIComponent(query)}&page=${page}&per_page=12`, {
-//       headers: {
-//         Authorization: `token ${GITHUB_TOKEN}`,
-//       },
-//     });
-
-//     if (!response.data.items || response.data.items.length === 0) {
-//       throw new Error("No users found with the given criteria");
-//     }
-
-//     const userDetailsPromises = response.data.items.map(async (user) => {
-//       const res = await axios.get(user.url, {
-//         headers: {
-//           Authorization: `token ${GITHUB_TOKEN}`,
-//         },
-//       });
-//       return res.data;
-//     });
-
-//     const usersDetailed = await Promise.all(userDetailsPromises);
-
-//     return {
-//       items: usersDetailed,
-//       total_count: response.data.total_count,
-//     };
-//   } catch (err) {
-//     throw new Error(err.response?.data?.message || "User not found or API limit exceeded");
-//   }
-// };
-
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com/search/users";
+const GITHUB_API_URL = "https://api.github.com/search/users?q";
 const GITHUB_TOKEN = import.meta.env.VITE_APP_GITHUB_API_KEY
 const headers = GITHUB_TOKEN ? { Authorization: `token ${GITHUB_TOKEN}` } : {};
 
